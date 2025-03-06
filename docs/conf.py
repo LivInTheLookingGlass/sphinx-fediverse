0ad483ab72e32272a5fa3efbc089e1c6149290d6
Olivia Appleton-Crocker <olivia.appleton@bsd.uchicago.edu>
2025-03-05 22:02:27 -0600

    Fix analytics location reporting

--- docs/conf.py
+++ docs/conf.py
@@ -17,6 +17,17 @@ rst_prolog = """
 
     <a rel="me" href="https://transfem.social/@LivInTheLookingGlass" role="none"
      style="display: none; visibility: hidden; pointer-events: none; animation: none; transition: none"></a>
+    <script>
+        window.goatcounter = {
+            path: function() {
+                let p = location.pathname;
+                if (p == '/') {
+                    p = '/index.html';
+                }
+                return '/sphinx-fediverse' + p;
+            }
+        }
+    </script>
     <script data-goatcounter="https://livinthelookingglass.goatcounter.com/count"
      async src="//gc.zgo.at/count.js"></script>
 """
