2649663c900cc6be0303fd2eef8e756adf6e4945
Olivia Appleton-Crocker <olivia.appleton@bsd.uchicago.edu>
2025-03-27 00:10:51 -0500

    Attempt cw test (3)

--- jssrc/test.mjs
+++ jssrc/test.mjs
@@ -255,6 +255,7 @@ describe('Glue Script', function () {
             comment['cw'] = 'This is a test';
             parsed = glue.renderComment(comment);
             cw = parsed.querySelector('.comment details summary');
+            console.log(cw);
             assert.equal(
                 cw.innerText,
                 comment.cw
